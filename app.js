7ac24a6d12d984134ce041e0d1bbc56827541d78
2apty507 <156866827+2apty507@users.noreply.github.com>
2024-02-15 22:41:50 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const caja = document.getElementById ("caja");
+const textoEncriptado = document.getElementById("textoencriptado");
+
+let codigo = [
+        ["e", "enter"],
+        ["o", "ober"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["u", "ufat"],
+]
+
+const remplazar = (all) =>{
+    textoEncriptado.innerHTML = all
+    copiarTexto.style.display ="inline-block"
+    
+}
+
+function encriptar(){
+    const texto = caja.value.toLowerCase();
+    function cifrar(resultado){
+        for( let i = 0; i < codigo.length; i++){
+            if(resultado.includes(codigo[i][0])){
+               resultado = resultado.replaceAll(codigo[i][0], codigo[i][1]);
+            };
+            
+        };
+        return resultado
+    };
+    remplazar(cifrar(texto)) 
+};
+
+function desencriptar(){
+    const texto = caja.value.toLowerCase();
+    function descifrar(resultado){
+        for( let i= 0; i < codigo.length; i++){
+            if(resultado.includes(codigo[i][1])){
+            resultado = resultado.replaceAll(codigo[i][1], codigo[i][0]);
+            };
+        };
+        return resultado
+    };
+    remplazar(descifrar(texto));
+};
+
+function copiar(){
+    let copiado = textoEncriptado;
+    copiado.select();
+    document.execCommand('copy');
+    //reeniciar o limpiar todo
+    caja.value =""
+    textoEncriptado.innerHTML = "";
+    copiarTexto.style.display ="none"
+    caja.focus();
+
+};
